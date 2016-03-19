5d7bdfa815080d44cfe949cb18516a6458214522
Cuong Nguyen <cuong.nguyen@niteco.se>
2016-03-19 16:13:25 +0700

    Change to bootstrap 3.1

--- client/app/app.js
+++ client/app/app.js
@@ -7,9 +7,19 @@ angular.module('techkidsApp', [
   'ngSanitize',
   'ui.router'
 ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController'
+      })
+      .state('aboutUs', {
+        url: '/about-us',
+        templateUrl: 'app/about-us/about-us.html'
+      });
 
     $locationProvider.html5Mode(true);
   });
